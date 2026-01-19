80b4c93aeb37fb50d83a8b5ed9de93cad407c6e6
vijayasankar22 <vijayasankar2225@gmail.com>
2026-01-19 14:56:42 +0530

    New Program added

--- src/SumAndAverage.java
+++ src/SumAndAverage.java
@@ -1,7 +1,7 @@
 public class SumAndAverage {
     public static void main(String[] args) {
         int[] arr = {25,456,58,6,4,96,2,9,4,5,9,1,6,12,3,5,2,6,23,56,2,65,2,65,2,65,2,65,2,5,62};
-        int sum=0, average=0;
+        int sum=0, average;
         for(int i:arr){
             sum+=i;
         }
