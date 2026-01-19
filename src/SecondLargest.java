b030da3a153e5fa42dbd231ac2b07930a07d2295
vijayasankar22 <vijayasankar2225@gmail.com>
2026-01-19 15:11:23 +0530

    New Program added

---
+++ src/SecondLargest.java
@@ -0,0 +1,18 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {25,-456,-58,6,44,942,2,9,4,5,459,1,1526,12,2423,5,242,6,2423,561,2,65,2,65,2,65,2,65,2,5,62};
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+        for(int i:arr){
+            if(i>max)
+            {
+                smax=max;
+                max=i;
+            }
+            else if(i>smax && i!=max){
+                smax=i;
+            }
+        }
+        System.out.println(smax);
+    }
+}
