3966c51691464a0893971f90a8daa81505c082e4
vijayasankar22 <vijayasankar2225@gmail.com>
2026-01-20 10:54:37 +0530

    New Program added

--- Arrays/MergeSorted.java
+++ Arrays/MergeSorted.java
@@ -1,7 +1,25 @@
+import java.util.Arrays;
+
 public class MergeSorted {
     public static void main(String[] args) {
         int[] a={2,5,9,12,18,26,35,41};
         int[] b={1,5,6,19,22,30,41,56};
+        int[] c= new int[a.length+b.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<a.length && j<b.length){
+                    if(a[i]<=b[j]) c[k++]=a[i++];
+                            else c[k++]= b[j++];
+                }
+        while (i < a.length) {
+            c[k++] = a[i++];
+        }
 
+        while (j < b.length) {
+            c[k++] = b[j++];
+        }
+
+        System.out.println(Arrays.toString(c));
     }
 }
