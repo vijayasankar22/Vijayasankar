fa67b0346095ceb77b2fd1ec14f06743eb064d24
CodeCraftsMan-Jr <vasanthan22td0728@svcet.ac.in>
2026-01-23 14:55:48 +0530

    Learnt New concept In Core Java

---
+++ Core java/WrapperClass.java
@@ -0,0 +1,18 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        Integer a = new Integer(45);
+        System.out.println(a);
+        Byte b = 10;
+        Integer c= 25;
+
+        Boolean x = new Boolean(true);
+
+        if(x) System.out.println("Yes True");
+
+        int v = Integer.parseInt("125");
+        System.out.println(v);
+
+
+
+    }
+}
